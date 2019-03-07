18e3a748122111ac7fce79d35611b49cc39cfc02
marty-p <marty-p@outlook.com>
2019-03-07 22:27:43 +0100

    UIManager DisplayGameOverText

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,39 @@
+﻿// This script is a Manager that controls the UI HUD (deaths, time, and orbs) for the 
+// project. All HUD UI commands are issued through the static methods of this class
+
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    //This class holds a static reference to itself to ensure that there will only be
+    //one in existence. This is often referred to as a "singleton" design pattern. Other
+    //scripts access this one through its public static methods
+    static UIManager current;
+    public TextMeshProUGUI gameOverText;    //Text element showing the Game Over message
+
+    void Awake()
+    {
+        //If an UIManager exists and it is not this...
+        if (current != null && current != this)
+        {
+            //...destroy this and exit. There can be only one UIManager
+            Destroy(gameObject);
+            return;
+        }
+
+        //This is the current UIManager and it should persist between scene loads
+        current = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static void DisplayGameOverText()
+    {
+        //If there is no current UIManager, exit
+        if (current == null)
+            return;
+
+        //Show the game over text
+        current.gameOverText.enabled = true;
+    }
+}
