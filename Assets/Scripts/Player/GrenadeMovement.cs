2ff661d6cb9b3e51e8db9585eae0a2b2f6896c7d
marty-p <marty-p@outlook.com>
2019-03-12 11:17:48 +0100

    renamed Granate to Grenade

--- Assets/Scripts/Player/GrenadeMovement.cs
+++ Assets/Scripts/Player/GrenadeMovement.cs
@@ -2,24 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Granate_movement : MonoBehaviour
+public class GrenadeMovement : MonoBehaviour
 {
 
     private Rigidbody2D rb;
    
     //public float lifeTime = 5;
-    private float damageGranate = 300;
-    public float granateForce;
-    public Animator granateAnimator;
-    public GameObject granateSpawn;
-    Vector3 granateDirection;
+    private float damageGrenade = 300;
+    public float grenadeForce;
+    public Animator grenadeAnimator;
+    public GameObject grenadeSpawn;
+    Vector3 grenadeDirection;
     private Vector2 startingPoint;
     private Vector2 controlPoint;
     private Vector2 endingPoint;
 
     public float aoeRangeX;
     public float aoeRangeY;
-    public LayerMask whatIsEnemy;
 
     // Start is called before the first frame update
     void Start()
@@ -28,20 +27,20 @@ public class Granate_movement : MonoBehaviour
         switch (rb.rotation)
         {
             case 0:
-                granateDirection = Quaternion.AngleAxis(45, Vector3.forward) * Vector3.right;
+                grenadeDirection = Quaternion.AngleAxis(45, Vector3.forward) * Vector3.right;
                 break;
             case 180:
-                granateDirection = Quaternion.AngleAxis(-45, Vector3.forward) * Vector3.left;
+                grenadeDirection = Quaternion.AngleAxis(-45, Vector3.forward) * Vector3.left;
                 break;
             case -90:
-                granateDirection = Quaternion.AngleAxis(-45, Vector3.forward) * Vector3.left;
+                grenadeDirection = Quaternion.AngleAxis(-45, Vector3.forward) * Vector3.left;
                 break;
             case 90:
-                granateDirection = Quaternion.AngleAxis(45, Vector3.forward) * Vector3.right;
+                grenadeDirection = Quaternion.AngleAxis(45, Vector3.forward) * Vector3.right;
                 break;
         }
         rb.rotation = 0;
-        rb.AddForce(granateDirection * granateForce, ForceMode2D.Impulse);
+        rb.AddForce(grenadeDirection * grenadeForce, ForceMode2D.Impulse);
 
     }
 
@@ -64,28 +63,28 @@ public class Granate_movement : MonoBehaviour
         {
             if (collision.tag == "Enemy" || collision.tag == "Building" || collision.tag == "Boat" || collision.tag == "Terrain" || collision.tag == "Walkable")
             {
-                granateAnimator.SetBool("hasHittenSth", true);
-                Collider2D[] thingsToDamage = Physics2D.OverlapBoxAll(rb.position, new Vector2(aoeRangeX, aoeRangeY), 0, whatIsEnemy);
+                grenadeAnimator.SetBool("hasHittenSth", true);
+                Collider2D[] thingsToDamage = Physics2D.OverlapBoxAll(rb.position, new Vector2(aoeRangeX, aoeRangeY), 0, GameManager.GetEnemyLayer());
                 foreach (Collider2D thing in thingsToDamage)
                 {
                     if(thing.tag == "Enemy")
                     {
-                        thing.GetComponent<EnemyControl>().Hit(damageGranate);
+                        thing.GetComponent<EnemyControl>().Hit(damageGrenade);
                     }
                     else if(thing.tag == "Building")
                     {
-                        thing.GetComponent<BuildingController>().Hit(damageGranate);
+                        thing.GetComponent<BuildingController>().Hit(damageGrenade);
                     }
                     else if(thing.tag == "Boat")
                     {
-                        thing.GetComponent<BoatController>().Hit(damageGranate);
+                        thing.GetComponent<BoatController>().Hit(damageGrenade);
                     }
                 }
 
                 this.rb.rotation = 0;
                 Destroy(rb);
                 yield return new WaitForSeconds(1.7f);
-                granateAnimator.SetBool("hasHittenSth", false);
+                grenadeAnimator.SetBool("hasHittenSth", false);
                 Destroy(gameObject);
             }
         }
