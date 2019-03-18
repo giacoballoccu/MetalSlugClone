6fff9b5124cdcfda51138f41437ddba6b9c7b52b
marty-p <marty-p@outlook.com>
2019-03-18 10:34:39 +0100

    m1 2za done

---
+++ Assets/Scripts/Mission1/AfterTheSecondCartel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfterTheSecondCartel : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            CameraManager.AfterMarcoBoatCartel();
+            Destroy(gameObject);
+        }
+    }
+}
