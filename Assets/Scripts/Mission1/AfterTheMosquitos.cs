50eaab499cbac6b59b49e6bcb91e4b688adcfb89
marty-p <marty-p@outlook.com>
2019-03-18 10:27:43 +0100

    m1 1ze done

---
+++ Assets/Scripts/Mission1/AfterTheMosquitos.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfterTheMosquitos : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            CameraManager.AfterMosquitos();
+            Destroy(gameObject);
+        }
+    }
+}
