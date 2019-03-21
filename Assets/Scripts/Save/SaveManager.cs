fe22f14fcc34ba0cb50a32232e5f6bbc2cecac65
Martino Pinna <37085112+marty-p@users.noreply.github.com>
2019-03-21 09:54:39 +0100

    Update SaveManager.cs

--- Assets/Scripts/Save/SaveManager.cs
+++ Assets/Scripts/Save/SaveManager.cs
@@ -36,16 +36,12 @@ public class SaveManager : MonoBehaviour
         // Application.StreamingAssets points to Assets/StreamingAssets in the Editor, and the StreamingAssets folder in a build
         string filePath = Path.Combine(Application.streamingAssetsPath, settingsFilename);
 
-        if(File.Exists(filePath))
+        if (File.Exists(filePath))
         {
             // Read the json from the file into a string
             string dataAsJson = File.ReadAllText(filePath); 
             // Pass the json to JsonUtility, and tell it to create a GameData object from it
             settings = JsonUtility.FromJson<Settings>(dataAsJson);
-        }
-        else
-        {
-            // Debug.LogError("Cannot load settings data!");
         }
     }
 }
