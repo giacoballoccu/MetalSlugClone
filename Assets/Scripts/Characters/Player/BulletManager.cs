bec771703ab65efa538cea5e5c4606676747c386
marty-p <marty-p@outlook.com>
2019-03-23 19:54:10 +0100

    BulletManager commented

--- Assets/Scripts/Characters/Player/BulletManager.cs
+++ Assets/Scripts/Characters/Player/BulletManager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class BulletManager : MonoBehaviour
 {
+    [Header("Prefabs Details")]
     [SerializeField] public GameObject _normalBullet;
     [SerializeField] public GameObject _heavyMachineBullet;
     [SerializeField] public GameObject _grenade;
     [SerializeField] public int _poolCount = 10;
 
-    static protected Dictionary<GameObject, BulletPool> _pools = new Dictionary<GameObject, BulletPool>();
-    static BulletManager _instance;
+    static protected Dictionary<GameObject, BulletPool> _pools = new Dictionary<GameObject, BulletPool>(); // <prefab, pool>
+    static BulletManager _instance; // singleton
 
     void Awake()
     {
@@ -19,6 +20,7 @@ public class BulletManager : MonoBehaviour
 
     void Start()
     {
+        // instantiate the <prefab, pool> dictionaries if assigned from the editor
         if (_normalBullet)
             _pools[_normalBullet] = new BulletPool(_normalBullet);
         if (_heavyMachineBullet)
@@ -61,6 +63,7 @@ public class BulletManager : MonoBehaviour
         List<GameObject> _actives = new List<GameObject>();
         List<GameObject> _inactives = new List<GameObject>();
 
+        // 1) create n prefab instances, 2) set active to false, 3) and add them into the inactives list
         public BulletPool(GameObject prefab)
         {
             _prefab = prefab;
@@ -72,6 +75,7 @@ public class BulletManager : MonoBehaviour
             }
         }
 
+        // 1) pop from inactives list an element or generate a new one if missing, 2) then push it to actives list
         public void Spawn(Vector2 position, Quaternion rotation)
         {
             GameObject bullet = null;
@@ -82,12 +86,14 @@ public class BulletManager : MonoBehaviour
             PushToActives(position, rotation, bullet);
         }
 
+        // 1) set element visible with new position & rotation, 2) then add it to actives list
         private void PushToActives(Vector2 position, Quaternion rotation, GameObject bullet)
         {
             BulletSetVisible(bullet, position, rotation);
             _actives.Add(bullet);
         }
 
+        // remove the last element from inactives list and return it
         private GameObject PopFromInactives()
         {
             GameObject bullet;
@@ -97,6 +103,7 @@ public class BulletManager : MonoBehaviour
             return bullet;
         }
 
+        // 1) reset bullet rb and hide it, 2) add it to inactives, 3) remove it from actives
         public void Despawn(GameObject bullet)
         {
             BulletReset(bullet);
@@ -107,6 +114,7 @@ public class BulletManager : MonoBehaviour
             _actives.RemoveAt(id);
         }
 
+        // instantiate on BulletManager container, and reset (hide) it
         private GameObject BulletNew()
         {
             GameObject bullet = Instantiate(_prefab, _instance.transform);
@@ -114,6 +122,7 @@ public class BulletManager : MonoBehaviour
             return bullet;
         }
 
+        // switch position & rotation, then activate it
         private void BulletSetVisible(GameObject bullet, Vector2 position, Quaternion rotation)
         {
             bullet.transform.position = position;
@@ -121,6 +130,7 @@ public class BulletManager : MonoBehaviour
             bullet.SetActive(true);
         }
 
+        // reset rb velocity, then disable the element
         private void BulletReset(GameObject bullet)
         {
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
