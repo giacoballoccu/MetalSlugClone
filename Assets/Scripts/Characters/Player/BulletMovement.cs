b85d860be617c1800483f6d1cadce89e01d6af1b
marty-p <marty-p@outlook.com>
2019-03-23 18:52:18 +0100

    BulletPool stable

--- Assets/Scripts/Characters/Player/BulletMovement.cs
+++ Assets/Scripts/Characters/Player/BulletMovement.cs
@@ -9,27 +9,40 @@ public class BulletMovement : MonoBehaviour
     public float bulletForce = 3;
     public float lifeTime = 5;
     public float damageShot = 100;
+    private float expireTime;
+    private bool isSpawned;
 
-    void Start()
+    void OnEnable()
     {
-        rb = GetComponent<Rigidbody2D>();
+        Init();
+    }
 
+    private void Init()
+    {
+        expireTime = lifeTime;
         Vector3 bulletDirection = transform.right;
-
+        rb = GetComponent<Rigidbody2D>();
         rb.AddForce(bulletDirection * bulletForce, ForceMode2D.Impulse);
+        isSpawned = true;
     }
 
     void Update()
     {
-        lifeTime -= Time.deltaTime;
-        if (lifeTime <= 0)
-            Destroy(this.gameObject);
+        if (!isSpawned)
+            return;
+
+        expireTime -= Time.deltaTime;
+        if (expireTime <= 0)
+        {
+            isSpawned = false;
+            BulletManager.GetNormalBulletPool().Despawn(this.gameObject);
+        }
     }
 
     //Destroy the bulled when out of camera
     private void OnBecameInvisible()
     {
-        Destroy(gameObject);
+        BulletManager.GetNormalBulletPool().Despawn(this.gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -38,13 +51,13 @@ public class BulletMovement : MonoBehaviour
         {
             collision.gameObject.GetComponent<Health>().Hit(damageShot);
             AudioManager.PlayShotHitAudio();
-            Destroy(gameObject);
+            BulletManager.GetNormalBulletPool().Despawn(this.gameObject);
         }
         else if (collision.CompareTag("Building"))
         {
             collision.gameObject.GetComponent<Health>().Hit(damageShot);
             AudioManager.PlayShotHitAudio();
-            Destroy(gameObject);
+            BulletManager.GetNormalBulletPool().Despawn(this.gameObject);
         }
     }
 }
