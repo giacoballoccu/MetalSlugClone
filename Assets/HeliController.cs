a6a3bdb0bc72f32df40b94c4022780ff30c8101e
Emanuele Concas <conema@outlook.com>
2019-03-28 15:00:09 +0100

    Marco fix spawn bullet and granade

--- Assets/HeliController.cs
+++ Assets/HeliController.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class HeliController : MonoBehaviour
 {
     public float speed = 0.5f;
+    public bool facingRight = false;
+
     Rigidbody2D rb;
 
     private int changeDirectionX = -1;
     private int changeDirectionY = 0;
-    public bool facingRight = false;
-
     private bool flipped = false;
+
     private float height;
+    private Animator animator;
+    private BlinkingSprite blinkingSprite;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        blinkingSprite = GetComponent<BlinkingSprite>();
+
         height = rb.position.y;
     }
 
@@ -68,5 +74,13 @@ public class HeliController : MonoBehaviour
 
         //transform.localEulerAngles = transform.eulerAngles + new Vector3(0, 180, -2 * transform.eulerAngles.z);
         facingRight = !facingRight;
+    }
+
+    private void OnHit(float damage)
+    {
+        animator.SetTrigger("isHitten");
+
+        GameManager.AddScore(damage);
+        blinkingSprite.Play();
     }
 }
