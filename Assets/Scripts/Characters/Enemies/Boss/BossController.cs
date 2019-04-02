d15ca0b0bc054b04d029002ba8c87f271a1f6475
giacoballoccu <37103199+giacoballoccu@users.noreply.github.com>
2019-04-02 20:36:35 +0200

    boss script pt1

---
+++ Assets/Scripts/Characters/Enemies/Boss/BossController.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+
+    [Header("Enemy information")]
+    GameObject followPlayer;
+    public float speed = 0.5f;
+    public float attackDamage = 10f;
+    public bool isMovable = true;
+    public bool canMelee = true;
+    public AudioClip deathClip;
+    private Health health;
+    private BlinkingSprite blinkingSprite;
+    public GameObject projSpawner;
+
+    [Header("Throwable")]
+    public GameObject throwableObj;
+    public bool canThrow = false;
+
+    [Header("Enemy activation")]
+    public float activationDistance = 1.8f;
+    public float attackDistance = 0.7f;         //Far attack
+    public float meleeDistance = 0.5f;          //Near attack
+    public const float CHANGE_SIGN = -1;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    public bool facingRight = false;
+
+    //Enemy gravity
+    private bool collidingDown = false;
+    Vector2 velocity = Vector2.zero;
+
+    [Header("Time shoot")]
+    private float shotTime = 0.0f;
+    public float fireDelta = 0.5f;
+    private float nextFire = 0.5f;
+
+    private bool canFall = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+        followPlayer = GameManager.GetPlayer();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        blinkingSprite = GetComponent<BlinkingSprite>();
+        registerHealth();
+    }
+
+    private void registerHealth()
+    {
+        health = GetComponent<Health>();
+        // register health delegate
+        health.onDead += OnDead;
+        health.onHit += OnHit;
+        if(health.GetHealth() <= health.GetHealth() / 2)
+        {
+            StartCoroutine(HalfHealth());
+        }
+    }
+
+    private void OnDead(float damage)
+    {
+        StartCoroutine(Die());
+    }
+
+    private void OnHit(float damage)
+    {
+        GameManager.AddScore(damage);
+        blinkingSprite.Play();
+    }
+
+    private IEnumerator Die()
+    {
+        animator.SetBool("isDying", true);
+        if (rb)
+            rb.isKinematic = true;
+        if (GetComponent<BoxCollider2D>())
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+        else if (GetComponent<CapsuleCollider2D>())
+        {
+            GetComponent<CapsuleCollider2D>().enabled = false;
+        }
+
+        yield return new WaitForSeconds(1.8f);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(followPlayer.transform.position.x == 20.8)
+        {
+            Spawn();
+        }
+
+
+        
+    }
+
+    void Spawn()
+    {
+        rb.AddForce(new Vector2(0f, 5f));
+    }
+
+    private IEnumerator HalfHealth()
+    {
+        animator.SetBool("isHalfHealth", true);
+        yield return new WaitForSeconds(0.11f);
+    }
+}
