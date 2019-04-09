734d7f995ad30af41952551fc493a140479ae2fc
marty-p <marty-p@outlook.com>
2019-04-09 15:19:19 +0200

    easy and hard mode health code added

--- Assets/Scripts/Characters/Health.cs
+++ Assets/Scripts/Characters/Health.cs
@@ -6,11 +6,22 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 100;
     public float health;
+    private bool isPlayer;
 
     public delegate void OnDamageEvent(float damage);    public OnDamageEvent onHit;    public OnDamageEvent onDead;
 
     void Start()
     {
+        isPlayer = GetComponent<PlayerController>() != null; // PlayerController is just for player
+        var difficulty = GameManager.GetDifficultyMode(); // get difficulty
+
+        if (!isPlayer) // only if enemy
+        {
+            if (difficulty == GameManager.Difficulty.Easy) // hp - 30%
+                maxHealth *= 0.7f;
+            else if(difficulty == GameManager.Difficulty.Hard) // hp + 30%
+                maxHealth *= 1.3f;
+        }
         health = maxHealth;
     }
 
@@ -42,7 +53,16 @@ public class Health : MonoBehaviour
         if (!IsAlive()) // skip already dead
             return;
 
+        if (isPlayer) // only if player
+        {
+            var difficulty = GameManager.GetDifficultyMode(); // get difficulty
+            if (difficulty == GameManager.Difficulty.Easy) // dmg - 30%
+                damage *= 0.7f;
+            else if (difficulty == GameManager.Difficulty.Hard) // dmg + 30%
+                damage *= 1.3f;
+        }
         health -= damage; // take the hit
+
         onHit?.Invoke(damage);
 
         if (!IsAlive()) // check if died
