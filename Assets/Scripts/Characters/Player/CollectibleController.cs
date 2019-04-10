738a9d13e823cf46d3b3a64a414af83b8f49bd12
marty-p <marty-p@outlook.com>
2019-04-10 20:25:32 +0200

    collectibles will now give 1000 points as well

--- Assets/Scripts/Characters/Player/CollectibleController.cs
+++ Assets/Scripts/Characters/Player/CollectibleController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class CollectibleController : MonoBehaviour
 {
     public PlayerController.CollectibleType type = PlayerController.CollectibleType.HeavyMachineGun;
+    private int collectiblePoints = 1000;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
         {
             collision.collider.GetComponent<PlayerController>().getCollectible(type);
+            GameManager.AddScore(collectiblePoints);
             Destroy(gameObject);
         }
     }
