ce03604d6ade0bc543ddd1c397b9a1d81c3282ed
marty-p <marty-p@outlook.com>
2019-04-12 20:37:56 +0200

    boss2 prefabs and LineController

---
+++ Assets/LineController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+    private float chargingTime = 1.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestroyAfter());
+    }
+
+ private IEnumerator DestroyAfter()
+    {
+        yield return new WaitForSeconds(chargingTime);
+
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/RockController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockController : MonoBehaviour
+{
+    private Animator animator;
+    private Collider2D colllider;
+    private bool hitten = false;
+    public float damage = 50f;
+    private float animationTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Health>().onHit(damage);
+        }
+        if (collision.CompareTag("Bullet") || collision.CompareTag("Granate"))
+        {
+            StartCoroutine(rockHitten());
+        }
+    }
+
+    private IEnumerator rockFalling()
+    {
+        animator.SetBool("isGrounded", true);
+        yield return new WaitForSeconds(3.2f);
+        animator.SetBool("isGrounded", false);
+        Destroy(this);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Walkable"))
+        {
+            StartCoroutine(rockFalling());
+        }
+
+    }
+
+    private IEnumerator rockHitten()
+    {
+        animator.SetBool("isHitten", true);
+        yield return new WaitForSeconds(0.6f);
+        animator.SetBool("isHitten", false);
+        Destroy(this);
+    }
+}
