6cbb1ae4c288b9333ed411d0be63c5eff94cb20f
marty-p <marty-p@outlook.com>
2019-04-12 20:39:58 +0200

    bosscontroller2

---
+++ Assets/Scripts/Characters/Enemies/Boss/Boss2/TopController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopController : MonoBehaviour
+{
+    private Health health;
+    private BlinkingSprite blinkingSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blinkingSprite = GetComponent<BlinkingSprite>();
+        registerHealth();
+    }
+
+    private void registerHealth()
+    {
+        health = GetComponent<Health>();
+    }
+
+
+
+}
