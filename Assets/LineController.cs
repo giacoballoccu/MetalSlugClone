b9707a6096342651cee96773a5bfee152fd11243
giacoballoccu <37103199+giacoballoccu@users.noreply.github.com>
2019-04-12 20:29:56 +0200

    smallfix

--- Assets/LineController.cs
+++ Assets/LineController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LineController : MonoBehaviour
 {
-    private float chargingTime = 1.25f;
+    private float chargingTime = 2f;
     // Start is called before the first frame update
     void Start()
     {
